1942d8819a66ff8dc4d976196fcee8d38a608127
Derek Brans <dbrans@gmail.com>
2012-10-18 11:53:51 -0700

    fix for win.eval (inline scripts)

--- postscribe.js
+++ postscribe.js
@@ -272,10 +272,6 @@
       var doc = this.doc = this.root.ownerDocument;
 
       var win = this.win = doc.defaultView || doc.parentWindow;
-      // Creates win.eval in IE. I can't remember where I saw this trick.
-      if( win.execScript && !win['eval'] ) {
-        win.execScript('0');
-      }
     }
 
     Worker.prototype.exec = function(task, done) {
@@ -286,7 +282,11 @@
 
     Worker.prototype.script_inline = function(task, done) {
       try {
-        this.win['eval'](task.expr);
+        if(this.win.execScript) {
+          this.win.execScript(task.expr);
+        } else {
+          this.win['eval'](task.expr);
+        }
       } catch(e) {
         // TODO: this.options.error(e);
       }
