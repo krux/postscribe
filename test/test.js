86d4af33ebe711901ad94a2fc366ff174f7c2b16
jnewman <joshua.newman@krux.com>
2014-03-07 16:59:34 -0800

    Overwrites doc.open/close while postscribe runs.

--- test/test.js
+++ test/test.js
@@ -132,39 +132,44 @@ $(document).ready(function(){
   });
 
   module('document.write overwriting.');
-  function readNativeDocumentWriteString() {
-    var result = readNativeDocumentWriteString.cache;
+  function readNativeDocumentMethodString(method) {
+    // Cache cause this takes a long time.
+    var cache = readNativeDocumentMethodString.cache;
+    if (!cache) {
+      cache = readNativeDocumentMethodString.cache = {};
+    }
+    var result = cache[method];
     if (result) {
       return result;
     }
     var iframe = document.createElement('iframe');
     document.body.appendChild(iframe);
-    result = readNativeDocumentWriteString.cache = String(iframe.contentDocument.write);
+    result = cache[method] = String(iframe.contentDocument[method]);
     iframe.parentNode.removeChild(iframe);
     return result;
   }
 
-  function isDocumentWriteNative() {
-    return String(document.write) === readNativeDocumentWriteString();
+  function isNative(method) {
+    return String(document[method]) === readNativeDocumentMethodString(method);
   }
 
   // Must be async to avoid polluting doc.write for the next tests.
   asyncTest('overrides document.write for normal scripts.', 2, function() {
-    ok(isDocumentWriteNative());
+    ok(isNative('write'));
     postscribe(document.body, '<script src="remote/describe-write.js"></script>', {
       releaseAsync: true,
       done: start
     });
-    ok(!isDocumentWriteNative());
+    ok(!isNative('write'));
   });
 
   asyncTest('does not override document.write for async scripts.', 2, function() {
-    ok(isDocumentWriteNative());
+    ok(isNative('write'));
     postscribe(document.body, '<script async src="remote/describe-write.js"></script>', {
       releaseAsync: true,
       done: start
     });
-    ok(isDocumentWriteNative());
+    ok(isNative('write'));
   });
 
   asyncTest('afterAsync fires when async ignored.', 1, function() {
@@ -185,6 +190,24 @@ $(document).ready(function(){
         start();
       }
     });
+  });
+
+  asyncTest('overrides and returns writeln, open, and close', function() {
+    function all(assertion) {
+      var methods = ['writeln', 'open', 'close'];
+      for (var i = 0, len = methods.length; i < len; ++i) {
+        assertion(isNative(methods[i]));
+      }
+    }
+
+    all(ok);
+    postscribe(document.body, '<script src="remote/describe-write.js"></script>', {
+      afterAsync: function() {
+        all(ok);
+        start();
+      }
+    });
+    all(function(val) {return ok(!val);});
   });
 
   module('multiple');
