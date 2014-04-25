04a0a06ec3dae766a46cebc8d706fc117e3f606c
jnewman <joshua.newman@krux.com>
2014-04-25 10:25:46 -0700

    #30 - broken test for nested tables.

---
+++ test/remote/nested-table.js
@@ -0,0 +1,25 @@
+/*globals document*/
+/*jshint evil:true*/
+document.write('<table width=\'90%\' cellspacing=\'0\' cellpadding=\'10\' border=\'2\' >\n');
+document.write('<tbody>\n');
+document.write('<tr>\n');
+document.write('	<td>\n');
+
+document.write('		<table width=\'80%\' cellspacing=\'0\' cellpadding=\'0\' border=\'1\'>\n');
+document.write('		<tbody>\n');
+document.write('		<tr>\n');
+
+document.write('			<td>\n');
+document.write('				&nbsp;\n');
+document.write('			</td>\n');
+document.write('			<td>&nbsp;\n');
+document.write('			</td>\n');
+
+document.write('		</tr>\n');
+document.write('		</tbody>\n');
+document.write('		</table>\n');
+
+document.write('	</td>\n');
+document.write('</tr>\n');
+document.write('</tbody>\n');
+document.write('</table>\n');
