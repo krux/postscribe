0ac2fe63089157dd30918b428dd41919e3303b0b
jnewman <joshua.newman@krux.com>
2016-04-15 12:01:23 -0500

    Fix errors.spec

--- test/unit/errors.spec.js
+++ test/unit/errors.spec.js
@@ -2,26 +2,23 @@ import postscribe from '../../src/postscribe';
 
 describe('errors', () => {
   function handlesError(html) {
-    return done => {
+    // Naming finish b/c the callback to postscribe is already called done
+    return finish => {
       let oldOnError = window.onerror;
       window.onerror = null;
 
       const div = document.createElement('div');
       document.body.appendChild(div);
 
-      let error = null;
-
       postscribe(div, html, {
-        error(e) {
-          error = e;
+        error(err) {
           window.onerror = oldOnError;
-          expect(error).to.be.ok();
-          done();
+          expect(err).to.be.ok();
+          finish();
         },
         done() {
           window.onerror = oldOnError;
-          expect(error).to.not.be.ok();
-          done();
+          finish();
         }
       });
     };
